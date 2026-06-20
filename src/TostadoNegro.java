43b24d7bd8b7ec04d9a1502aa9441641a92e4bcd
Susana Bazan <subaez2810@gmail.com>
2026-06-20 17:11:22 -0500

    feat: agregar componente TostadoNegro

---
+++ src/TostadoNegro.java
@@ -0,0 +1,11 @@
+public class TostadoNegro implements Bebida {
+    @Override
+    public String descripcion() {
+        return "Tostado Negro";
+    }
+
+    @Override
+    public double precio() {
+        return 0.99;
+    }
+}
