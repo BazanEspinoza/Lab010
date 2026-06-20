26cf65a2f727dcd74bce22ea773ab6dd49fb8489
Susana Bazan <subaez2810@gmail.com>
2026-06-20 17:12:00 -0500

    feat: agregar componente Descafeinado

---
+++ src/Descafeinado.java
@@ -0,0 +1,11 @@
+public class Descafeinado implements Bebida {
+    @Override
+    public String descripcion() {
+        return "Descafeinado";
+    }
+
+    @Override
+    public double precio() {
+        return 1.05;
+    }
+}
